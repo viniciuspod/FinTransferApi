fdcd163ff118cec15d9fff868c5f4b1df1ca7f39
viniciuspod <viniciuspodantas@gmail.com>
2024-06-26 17:04:46 -0300

    fisrt commit springInitializr

---
+++ src/main/java/br/com/tokiomarine/FinTransferApi/FinTransferApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.tokiomarine.FinTransferApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinTransferApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinTransferApiApplication.class, args);
+	}
+
+}
